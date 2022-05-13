f5e2adc1f8fc17767804a32a82c2fc3f28d3cbce
Omar Arenas <omararenasuni@gmail.com>
2022-05-13 06:40:08 -0500

    Feat: agregando nuevas clases y separando archivos

--- index.js
+++ index.js
@@ -20,79 +20,7 @@ const WASD = {
     d: { pressed: false },
     w: { pressed: false }
 }
-
-class Sprite {
-    constructor(position, speed, color, height, width, lastKey, attackBox, attacking, cooldown, life, damage, name) {
-        this.position = position;
-        this.speed = speed;
-        this.color = color;
-        this.height = height;
-        this.width = width;
-        this.lastKey = lastKey;
-        this.attackBox = attackBox;
-        this.attacking = attacking;
-        this.cooldown = cooldown;
-        this.life = life;
-        this.damage = damage;
-        this.name = name;
-    }
-
-    getPosition() {
-        return this.position;
-    }
-
-    setPosition(newPosition) {
-        try {
-            if (Object.values(newPosition).length === 2) {
-                this.position = newPosition;
-                return true;
-            } else {
-                console.error('El objeto position debe tener 2 variables numericas');
-
-            }
-        } catch (error) {
-            console.error('No se puede asignar ese objeto al position');
-            console.error("TYPE ERROR:" + error);
-        }
-    }
-
-    draw() {
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        if (this.attacking && (this.lastKey === 'a' || this.lastKey === 'ArrowLeft')) {
-            c.fillStyle = 'green'
-            c.fillRect(this.position.x + this.width, this.position.y + this.attackBox.y, -this.attackBox.x, -this.attackBox.y)
-        } else if (this.attacking && (this.lastKey === 'd' || this.lastKey === 'ArrowRight')) {
-            c.fillStyle = 'green'
-            c.fillRect(this.position.x, this.position.y, this.attackBox.x, this.attackBox.y)
-        }
-    }
-
-    update() {
-        this.position.x += this.speed.x;
-        this.position.y += this.speed.y;
-        this.draw()
-        if (this.position.y + this.height + this.speed.y >= canvas.height || this.position.y + this.height + this.speed.y <= this.height) {
-            this.speed.y = 0;
-        } else {    
-            this.speed.y += GRAVITY;
-        }
-        if (this.position.x <= 0) {
-            this.position.x=0
-            this.speed.x = 0
-        }else if( this.position.x + this.width >= canvas.width){
-            this.position.x=canvas.width-this.width
-            this.speed.x = 0
-        }
-    }
-
-    attack() {
-        this.attacking = true;
-        setTimeout(() => this.attacking = false, 500)
-    }
-}
-
-const player = new Sprite(
+const player = new Fighter(
     { x: 0, y: 0 },
     { x: 0, y: 10 },
     'royalblue',
@@ -101,12 +29,12 @@ const player = new Sprite(
     '',
     { x: 100, y: 50 },
     false,
-    1000,
+    false,
     { maxHealth: 100, currentLife: 100 },
     5,
     'PLAYER'
 )
-const enemy = new Sprite(
+const enemy = new Fighter(
     { x: 975, y: 0 },
     { x: 0, y: 10 },
     'red',
@@ -115,7 +43,7 @@ const enemy = new Sprite(
     '',
     { x: 100, y: 50 },
     false,
-    1000,
+    false,
     { maxHealth: 100, currentLife: 100 },
     5,
     'ENEMY'
@@ -154,9 +82,11 @@ window.addEventListener('keydown', (e) => {
                 enemy.lastKey = 'd'
                 break;
             case ' ':
+                if (enemy.cooldown) return e.preventDefault()
                 if (!enemy.attacking) enemy.attack()
                 break;
             case '0':
+                if (player.cooldown) return e.preventDefault()
                 if (!player.attacking) player.attack()
                 break;
             default:
@@ -287,10 +217,8 @@ const animate = () => {
         if (player.life.currentLife > 0) player.life.currentLife -= player.damage
         playerHealth.style.width = `${player.life.currentLife > 0 ? ((player.life.currentLife * player.life.maxHealth) / 100) : 0}%`
     }
-
-    if (enemy.life.currentLife <= 0 || player.life.currentLife <= 0) {
+    if ((enemy.life.currentLife <= 0 || player.life.currentLife <= 0) && info.style.opacity !== "1") {
         determinateWinner(player, enemy)
-
     }
 }
 
