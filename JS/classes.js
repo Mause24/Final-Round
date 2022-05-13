0e45269555ceb450a4816ed554ec07a23f5b944a
Omar Arenas <omararenasuni@gmail.com>
2022-05-13 09:01:13 -0500

    Feat: archivos de las clases y recursos

---
+++ JS/classes.js
@@ -0,0 +1,113 @@
+class Fighter {
+    constructor(position, speed, color, height, width, lastKey, attackBox, attacking, cooldown, life, damage, name) {
+        this.position = position;
+        this.speed = speed;
+        this.color = color;
+        this.height = height;
+        this.width = width;
+        this.lastKey = lastKey;
+        this.attackBox = attackBox;
+        this.attacking = attacking;
+        this.cooldown = cooldown;
+        this.life = life;
+        this.damage = damage;
+        this.name = name;
+    }
+
+    getPosition() {
+        return this.position;
+    }
+
+    setPosition(newPosition) {
+        try {
+            if (Object.values(newPosition).length === 2) {
+                this.position = newPosition;
+                return true;
+            } else {
+                console.error('El objeto position debe tener 2 variables numericas');
+
+            }
+        } catch (error) {
+            console.error('No se puede asignar ese objeto al position');
+            console.error("TYPE ERROR:" + error);
+        }
+    }
+
+    draw() {
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        if (this.attacking && (this.lastKey === 'a' || this.lastKey === 'ArrowLeft')) {
+            c.fillStyle = 'green'
+            c.fillRect(this.position.x + this.width, this.position.y + this.attackBox.y, -this.attackBox.x, -this.attackBox.y)
+        } else if (this.attacking && (this.lastKey === 'd' || this.lastKey === 'ArrowRight')) {
+            c.fillStyle = 'green'
+            c.fillRect(this.position.x, this.position.y, this.attackBox.x, this.attackBox.y)
+        }
+    }
+
+    update() {
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+        this.draw()
+        if (this.position.y + this.height + this.speed.y >= canvas.height || this.position.y + this.height + this.speed.y <= this.height) {
+            this.speed.y = 0;
+        } else {
+            this.speed.y += GRAVITY;
+        }
+        if (this.position.x <= 0) {
+            this.position.x = 0
+            this.speed.x = 0
+        } else if (this.position.x + this.width >= canvas.width) {
+            this.position.x = canvas.width - this.width
+            this.speed.x = 0
+        }
+    }
+
+    attack() {
+        this.attacking = true;
+        setTimeout(() => this.attacking = false, 200)
+        this.cooldown = true
+        this.resetAttack()
+    }
+
+    resetAttack() {
+        setTimeout(() => {
+            this.cooldown = false
+        }, 1000)
+    }
+}
+
+class Sprite {
+    constructor(position, height, width) {
+        this.position = position;
+        this.height = height;
+        this.width = width;
+    }
+
+    getPosition() {
+        return this.position;
+    }
+
+    setPosition(newPosition) {
+        try {
+            if (Object.values(newPosition).length === 2) {
+                this.position = newPosition;
+                return true;
+            } else {
+                console.error('El objeto position debe tener 2 variables numericas');
+
+            }
+        } catch (error) {
+            console.error('No se puede asignar ese objeto al position');
+            console.error("TYPE ERROR:" + error);
+        }
+    }
+
+    draw() { }
+
+    update() {
+        this.draw()
+    }
+
+
+}
